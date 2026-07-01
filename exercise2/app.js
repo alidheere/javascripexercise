6c99b12f25cd242f99ea8f689532597045500fbc
alidheere <sakajr33@gmail.com>
2026-06-30 21:35:04 -0700

    exercise2

---
+++ exercise2/app.js
@@ -0,0 +1,28 @@
+
+
+let a = 15;
+let b = "15";
+
+// equal t0
+console.log(a == b)
+
+// not equal to
+console.log(a != b)
+
+// strictly equal to
+console.log( a === b)
+
+// strictly not equal to
+console.log( a !== b)
+
+// greter than 
+console.log(a > b)
+
+// greter than or equal to
+console.log(a >= b)
+
+// less than 
+console.log(a < b)
+
+// less than or equal to
+console.log(a <= b)
