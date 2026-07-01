627b0e315563c932d9e5dddf9263fac833c10f11
alidheere <sakajr33@gmail.com>
2026-07-01 11:30:47 -0700

    exercise4

---
+++ exercise4/apps.js
@@ -0,0 +1,26 @@
+// funcation declaration
+
+function add (x,y) {
+    console.log( + x + y)
+}
+add(20,20)
+add(25,25)
+add(30,30)
+add(200,200)
+
+const adds = function(a,b) {
+    console.log( a + b)
+}
+
+adds(20,30)
+
+adds(300,200)
+
+adds(30,50)
+
+const calculateTotal = function(price , quantity) {
+    console.log(' total price $' + price *quantity)
+}
+
+calculateTotal(5,25)
+calculateTotal(20,5)
