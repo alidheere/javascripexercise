203deffda811fd646d234a538bd70626b4b899fb
alidheere <sakajr33@gmail.com>
2026-07-02 09:03:46 -0700

    exericise7

---
+++ exercise7/app.js
@@ -0,0 +1,12 @@
+
+let colors  =['blue' , 'green', 'red' ,]
+
+console.log(colors)
+
+console.log('push')
+colors.push('yellow')
+console.log(colors)
+
+console.log('after shift')
+colors.shift()
+console.log(colors)
