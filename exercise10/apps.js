58d433d11f72dd6921915c4836609c6d9b9f5854
alidheere <sakajr33@gmail.com>
2026-07-03 08:58:06 -0700

    exercise10

---
+++ exercise10/apps.js
@@ -0,0 +1,36 @@
+
+
+let library = [
+    {
+        title : "To Kill a Mockingbird",
+        author : "Harper Lee",
+        Year : '1960',
+    },
+       {
+        title : "The Great Gatsby",
+        author : "F. Scott Fitzgerald",
+        Year : '1925',
+    }
+
+
+
+]
+
+console.log(library[0].title)
+
+console.log(library[1].author)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
