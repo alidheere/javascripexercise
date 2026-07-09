b1a9461acd0aa193b7971ba72ab9fa9222a32a31
alidheere <sakajr33@gmail.com>
2026-07-09 10:32:38 -0700

    exercise32

---
+++ exercise32/main.js
@@ -0,0 +1,15 @@
+
+
+const header = document.querySelector('#title');
+
+console.log(header);
+
+
+const description = document.querySelectorAll('.desc');
+
+console.log(description);
+
+
+const botton = document.querySelector('button');
+
+console.log(botton)
